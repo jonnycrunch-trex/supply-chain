8c0c5242a2d687f8db33c7767fc000948f78245e
Wale O <wale@screenverve.com>
2016-06-12 18:02:35 -0500

    set up listening at port

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const
-  express = require('express')
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  PORT = 3000,
+  app = express()
+
+
+
+app.listen(PORT, function(){
+  console.log(`This is a transmission from ${PORT}`)
+})
