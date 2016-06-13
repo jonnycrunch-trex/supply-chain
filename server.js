716087f2c9bdc739c54996609e77705143531496
Wale O <wale@screenverve.com>
2016-06-13 01:33:47 -0500

    add stubbed routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+// general index route - probably will remove
 app.route('/')
   .get((req, res) => {
     console.log('Get route works')
@@ -15,6 +16,39 @@ app.route('/')
   .post((req, res) => {
     console.log(req.body)
     res.send('end of post process')
+  })
+
+app.route('/contract/:id')
+  .get((req, res) => {
+    res.send(`This happened at Contract ID #${req.params.id}`)
+    res.end()
+  })
+
+app.route('/tx/:id')
+  .get((req, res) => {
+    res.send(`This happened at Transaction ID #${req.params.id}`)
+    res.end()
+  })
+
+app.route('/tx/:id/send')
+  .post((req, res) => {
+    console.log('Transaction', req.params.id)
+    res.json(req.body)
+    res.end()
+  })
+
+app.route('/contract/:id/agree')
+  .post((req, res) => {
+    console.log('Contract', req.params.id)
+    res.json(req.body)
+    res.end()
+  })
+
+app.route('/tx/:id/call')
+  .post((req, res) => {
+    console.log('Transaction', req.params.id)
+    res.json(req.body)
+    res.end()
   })
 
 app.listen(PORT, function(){
