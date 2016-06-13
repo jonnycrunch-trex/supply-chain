6e35459946536d9562214319915364b6e9dcd37a
Wale O <wale@screenverve.com>
2016-06-12 20:34:59 -0500

    add rest verbs for index route

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const
   PORT = 3000,
   app = express()
 
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.route('/')
+  .get((req, res) => {
+    console.log('Get route works')
+    res.send('Very nice')
+  })
+  .post((req, res) => {
+    console.log(req.body)
+    res.send('end of post process')
+  })
 
 app.listen(PORT, function(){
   console.log(`This is a transmission from ${PORT}`)
